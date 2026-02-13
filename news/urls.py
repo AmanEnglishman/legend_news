92b57501594bd5e89b41ca3202e782cca19e797b
electrovenik <englishmanaman@gmail.com>
2026-02-13 18:18:22 +0600

    asd

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import home, contacts
+from .views import home, contacts, news_detail
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-
+    path('<int:news_id>/', news_detail, name='news_detail'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,3 +1,4 @@
+from django.db.models import F
 from django.shortcuts import render
 
 from .models import News
@@ -9,3 +10,8 @@ def home(request):
 def contacts(request):
     return render(request, 'contacts.html')
 
+def news_detail(request, news_id):
+    news = News.objects.get(id=news_id)
+    News.objects.filter(id=news_id).update(views=F('views') + 1)
+    news.refresh_from_db()
+    return render(request, 'news_detail.html', {'news': news})
