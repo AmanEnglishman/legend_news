94106fb51296144c33c488480b4571d922d65f38
electrovenik <englishmanaman@gmail.com>
2026-02-16 18:20:31 +0600

    ads

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import home, contacts, news_detail
+from .views import home, contacts, news_detail, categories, categories_news
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
     path('<int:news_id>/', news_detail, name='news_detail'),
+    path('categories/', categories, name='categories'),
+    path('categories/<int:category_id>/', categories_news, name='categories_news'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,8 @@
 from django.db.models import F
 from django.shortcuts import render
 
-from .models import News
+from .models import News, Category
+
 
 def home(request):
     news = News.objects.order_by('-created_at')
@@ -15,3 +16,11 @@ def news_detail(request, news_id):
     News.objects.filter(id=news_id).update(views=F('views') + 1)
     news.refresh_from_db()
     return render(request, 'news_detail.html', {'news': news})
+
+def categories(request):
+    categories = Category.objects.all()
+    return render(request, 'categories.html', {'categories': categories})
+
+def categories_news(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    return render(request, 'categories_news.html', {'news': news})
